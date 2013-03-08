24aa3db4ecf6e86242bd4a89e1d90a94fe5254f2
Martijn Blankestijn <martijnblankestijn@gmail.com>
2013-03-08 09:09:55 +0100

    Wijzigingen pom (groupId) websocket logging

--- websocket/src/main/java/nl/ordina/javaee7/server/InverterDataEndpoint.java
+++ websocket/src/main/java/nl/ordina/javaee7/server/InverterDataEndpoint.java
@@ -1,7 +1,10 @@
 package nl.ordina.javaee7.server;
 
 import javax.annotation.PostConstruct;
-import javax.websocket.*;
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
@@ -9,7 +12,6 @@ import java.lang.invoke.MethodHandles;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -18,34 +20,54 @@ import java.util.logging.Logger;
 @ServerEndpoint(value = "/inverter/{inverterId}")
 public class InverterDataEndpoint {
     private static final Logger LOG = Logger.getLogger(MethodHandles.lookup().lookupClass().getSimpleName());
-    private final Set<Session> sessions = Collections.synchronizedSet(new HashSet<Session>());
+
+    // Sessions should static to share with different clients
+    private static final Set<Session> sessions = Collections.synchronizedSet(new HashSet<Session>());
+
+    public InverterDataEndpoint() {
+        log("ctor");
+    }
 
     @PostConstruct
     public void init() {
-        LOG.log(Level.INFO, "Starting up websocket endpoint");
+        log("Starting up websocket endpoint");
     }
+
     @OnOpen
     public void onOpen(Session peer) {
-        LOG.info("Adding peer " + peer);
+        log(" Adding peer with id " + peer.getId() + ", aantal open " + peer.getOpenSessions().size());
+
         sessions.add(peer);
+
+        log("Added peer with id " + peer.getId() + ", aantal open " + peer.getOpenSessions().size());
     }
 
     @OnClose
     public void onClose(Session peer) {
-        LOG.info("Removing peer " + peer);
+        log("Removing peer " + peer);
         sessions.remove(peer);
     }
 
     @OnMessage
     public void message(@PathParam("inverterId") String inverterId, String message, Session client) {
-        LOG.info("Got message from " + client +" for " + inverterId + ": " + message);
+        log("Got message from " + client + " for " + inverterId + ": " + message);
         for (Session peer : sessions) {
-            try {
-                LOG.info("Sending message to " + peer);
-                peer.getBasicRemote().sendText("Reply from server: " + message);
-            } catch (IOException e) {
-                System.out.println("IO Exception writing to " + peer + " .... ignoring");
+            // broadcast only to the other clients
+            if (peer.equals(client)) {
+                System.out.println("Client equals found session, will not echo!");
+            } else {
+                try {
+                    log("Sending message to " + peer);
+                    peer.getBasicRemote().sendText(message);
+                } catch (IOException e) {
+                    System.out.println("IO Exception writing to " + peer + " .... ignoring");
+                }
             }
         }
     }
+
+    private void log(String msg) {
+        LOG.info("InverterDataEndpoint " + hashCode() + " - " + msg);
+    }
+
 }
