b8f1e930fe6c372ba68a252e3c138a737059ec9f
Martijn Blankestijn <martijnblankestijn@gmail.com>
2013-04-29 13:14:53 +0200

    Logging toevoegd en handling van errors

--- websocket/src/main/java/nl/ordina/javaee7/server/websockets/SimpleEndpoint.java
+++ websocket/src/main/java/nl/ordina/javaee7/server/websockets/SimpleEndpoint.java
@@ -1,34 +1,55 @@
 package nl.ordina.javaee7.server.websockets;
 
-import javax.websocket.OnClose;
-import javax.websocket.OnMessage;
-import javax.websocket.OnOpen;
-import javax.websocket.Session;
+import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  *
  */
 @ServerEndpoint(value = "/simple")
 public class SimpleEndpoint {
-  private static final Set<Session> sessions = Collections.synchronizedSet(new HashSet<Session>());
+  @OnOpen public void onOpen(Session client) { log("OPEN", client); }
 
-  @OnOpen public void onOpen(Session client) {  sessions.add(client);   }
+  @OnClose public void onClose(Session client) { log("CLOSE", client); }
 
-  @OnClose public void onClose(Session client) {  sessions.remove(client); }
+  @OnError public void onError(Session client, Throwable throwable) throws Exception { log(client, throwable); }
 
   @OnMessage
   public void message(String message, Session client) {
-    for (Session peer : sessions) {
-      try {
-        peer.getBasicRemote().sendText(message);
-      } catch (IOException e) {
-        System.out.println("IO Exception  on " + peer + " .... ignoring");
+    System.out.println("Received " + message + ", distribute to " + client.getOpenSessions().size());
+    for (Session peer : client.getOpenSessions()) {
+      if(peer.isOpen()) {
+        System.out.println("Sent message to " + peer);
+
+        try {
+          peer.getBasicRemote().sendText(message);
+        } catch (IOException | NullPointerException e) {
+          System.out.println("Got exception, closing peer");
+          close(peer);
+        }
       }
     }
   }
+
+  private void close(final Session peer) {
+    try {
+      peer.close();
+    } catch (Exception e1) {
+      System.out.println("ignoring erorr " + e1.toString());
+    }
+  }
+
+  private void log(final Session client, final Throwable throwable) throws IOException {
+    System.out.println("-----Client gave error " + client);
+    log("ERROR", client);
+    throwable.printStackTrace(System.out);
+    System.out.println("-----");
+  }
+
+  private void log(final String label, final Session client) {
+    System.out.println(label + ",   open: " + client.getOpenSessions().size());
+  }
+
+
 }
